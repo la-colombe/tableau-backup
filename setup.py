1d85ba035c3fd2f40f31822a1cff34999882c151
Andres Recalde <andres.recalde@lacolombe.net>
2019-02-06 14:03:32 -0500

    set up package

--- setup.py
+++ setup.py
@@ -1,15 +1,21 @@
 from setuptools import setup
 
-setup(name='tableau-backup',
+setup(name='tableau_backup',
       version='0.1',
       description='Tool to backup Tableau site to S3 as a ZIP file',
-      url='',
+      url='https://github.com/la-colombe/tableau-backup',
       author='Andres Recalde',
       author_email='andres@lacolombe.com',
       license='MIT',
-      packages=['tableau-backup'],
+      py_modules=['tableau_backup'],
+      packages=['tableau_backup'],
       install_requires=[
           'tableauserverclient',
           'boto3',
       ],
-      zip_safe=False)
+      zip_safe=False,
+      entry_points="""
+      [console_scripts]
+      tableau-backup=tableau_backup:main
+      """,
+)
